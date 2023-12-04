9d5b1a9778bf85535eeb1cf536b6587d94b3a418
vaughnjones-marcus <vaughnjones.marcus@gmail.com>
2023-12-04 13:41:12 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "@chakra-ui/button";
+import { Container } from "@chakra-ui/react";
+import { Routes } from 'react-router-dom';
+import UserPage from './pages/UserPage';
+import PostPage from './pages/PostPage';
+
+function App() {
+  return (
+  <Container maxW="620px">
+    <Routes>
+      <Route path="/:username"  element={<UserPage/> }/>
+      <Route path="/:username/post/:pid"  element={<PostPage/> }/>
+    </Routes>
+  </Container>
+  );
+}
+  
+
+export default App
